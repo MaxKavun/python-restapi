16d0ed003007d2057fcfd33f9914bbbb8fe80745
Max Kavun <maxkavun@outlook.com>
2019-08-15 10:38:28 +0000

    My restapi service

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Server Works! Hey you, again'
+
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+app.run(host='0.0.0.0', port=5000)
